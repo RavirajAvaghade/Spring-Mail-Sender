e918e2addee0b600a62fbf0e869fdbff6a5480f2
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-13 15:14:36 +0530

    initial commit

---
+++ src/main/java/com/nt/Springboot1MailSendingProgramApplication.java
@@ -0,0 +1,15 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1MailSendingProgramApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Springboot1MailSendingProgramApplication.class, args);
+	}
+
+}
